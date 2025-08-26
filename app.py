331c671c502e4f32ae9e3c9e7048293d9fd767b5
Nasir_Mehmood <nasirjani757571@gmail.com>
2025-08-26 12:44:00 +0500

    Update app.py

--- app.py
+++ app.py
@@ -583,7 +583,7 @@ def title_page():
         videos = cur.fetchall()
 
         # Get distinct channels for dropdown
-        cur.execute("SELECT DISTINCT channel_name FROM youtube_top_videos_new ORDER BY channel;")
+        cur.execute("SELECT DISTINCT channel_name FROM youtube_top_videos_new ORDER BY channel_name;")
         channels = [r[0] for r in cur.fetchall()]
 
         cur.close()
